e653adac624f3ea456ccdff76b53a5dbb33a4890
zhuyuyi <yyzhu@che300.com>
2021-06-08 10:32:37 +0800

    修复windows rollup打包错误问题、将readme完善

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,11 @@ export default (_) =>
     base: "./",
     build: {
       assetsInlineLimit: "1000000", // 不让woff字体类文件单独打出来
-      chunkSizeWarningLimit:'1000', // 设置单文件 超过1M警告
+      chunkSizeWarningLimit: '1000', // 设置单文件 超过1M警告
       rollupOptions: { // 多页面打包配置
         input: {
-          background: "background.html",
-          main: "main.html",
+          background: resolve(__dirname, "background.html"),
+          main: resolve(__dirname, "main.html"),
         },
         output: { // 输入的文件格式
           assetFileNames: "static/css/[name].css",
@@ -21,7 +21,7 @@ export default (_) =>
         },
       },
     },
-    resolve: { 
+    resolve: {
       alias: { // alias
         "@": resolve(__dirname, "src"),
       },
